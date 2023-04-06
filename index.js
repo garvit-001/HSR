8632b0cb4755554f24e96319e195dc950ef2ba7e
Garvit Agrawal <garvit.agrawal.che20@itbhu.ac.in>
2023-04-06 22:49:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
